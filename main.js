d6991a42a07e8fbcadb11519e4b883e0d6f213ce
joeberenshtain <107581127+joeberenshtain@users.noreply.github.com>
2022-06-15 22:27:38 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 const WIDTH = 1000
-const HEIGHT = 1000
-var vec1 = [ -1, 0, 
-             1, 4]
-var n = 11
-var f = 11
+const HEIGHT =1000
+var n = 25
+var f = 25
+
+const BACKGROUND = "#ffffff"
+const BACK_LINES = "#222222"
+const AXIS_LINES = "#000000"
+const TRANS_LINES = "#3f75b2"
+
 const UNITX = WIDTH/n   
 const UNITY = HEIGHT/f
 var c = document.getElementById("canvas");
@@ -11,9 +15,11 @@ c.width = WIDTH
 c.height = HEIGHT
 var ctx = c.getContext("2d");
 
+ctx.fillStyle = BACKGROUND;
+
 function backgroundLines() {
     ctx.lineWidth = 1
-
+    ctx.strokeStyle = BACK_LINES
     ctx.beginPath()
     
     for (let i = 0; i < n; i++) {
@@ -29,55 +35,44 @@ function backgroundLines() {
 }
 function transformLines() {
 
-    const OFFSETX0 = (HEIGHT*vec1[0])/(2*vec1[2])
-    const OFFSETY0 = (WIDTH *vec1[2])/(2*vec1[0])
+    const OFFSETX0 = (HEIGHT*matrix[0])/(2*matrix[2])
+    const OFFSETY0 = (WIDTH *matrix[2])/(2*matrix[0])
     
-    const OFFSETX1 = (HEIGHT*vec1[3])/(2*vec1[1])
-    const OFFSETY1 = (WIDTH *vec1[1])/(2*vec1[3])
+    const OFFSETX1 = (HEIGHT*matrix[3])/(2*matrix[1])
+    const OFFSETY1 = (WIDTH *matrix[1])/(2*matrix[3])
 
     var V2V = 0;
     var V1V = 0;
     var V1H = 0;
     var V2H = 0;
-    if (vec1[0]*vec1[3] - vec1[1]*vec1[2] !== 0) {
-        V2V = (vec1[2] - vec1[3]*vec1[0]/vec1[1])*UNITY
-        V1V = (vec1[3] - vec1[2]*vec1[1]/vec1[0])*UNITY
-        V1H = (vec1[1] - vec1[0]*vec1[3]/vec1[2])*UNITX
-        V2H = (vec1[0] - vec1[1]*vec1[2]/vec1[3])*UNITX
+    if (matrix[0]*matrix[3] - matrix[1]*matrix[2] !== 0) {
+        V2V = (matrix[2] - matrix[3]*matrix[0]/matrix[1])*UNITY
+        V1V = (matrix[3] - matrix[2]*matrix[1]/matrix[0])*UNITY
+        V1H = (matrix[1] - matrix[0]*matrix[3]/matrix[2])*UNITX
+        V2H = (matrix[0] - matrix[1]*matrix[2]/matrix[3])*UNITX
     }
-    console.log(V2V/UNITX, V1V/UNITX)
+
     ctx.lineWidth = 4
 
-    ctx.strokeStyle = "#AA00aa" //PURPLE
+    ctx.strokeStyle = TRANS_LINES
     ctx.beginPath()
     for (let i = -10; i < n+10; i++) {
         if (UNITY !== Infinity && UNITY !== -Infinity && V1H !== Infinity && V1H !== -Infinity) {   
-            console.log("UNIT: ", V1H/UNITY )
-            ctx.strokeStyle = "#FFFF00" //PURPLE
             
             ctx.moveTo(WIDTH/2 + OFFSETX0 + i*V1H, 0)
             ctx.lineTo(WIDTH/2 - OFFSETX0 + i*V1H, HEIGHT)
         }// DOWN
         else {
-            console.log("V1V", V1V)
             ctx.moveTo(0,     HEIGHT/2 + OFFSETY0 + i*V1V)
             ctx.lineTo(WIDTH, HEIGHT/2 - OFFSETY0 + i*V1V)
         } // LEFT
-    } // Creates Vertical lines.
-    ctx.stroke()
-
-    ctx.strokeStyle = "#00FFF0"
-    ctx.beginPath()
-    
+    } // Creates Vertical lines.    
     for (let i = -10; i < f+10; i++) {
-        console.log(" : ", V2V/UNITY)
         if (UNITY !== Infinity && UNITY !== -Infinity && V2V !== Infinity && V2V !== -Infinity) {   
-            console.log(V2V)
             ctx.moveTo(0,     HEIGHT/2 + OFFSETX1 + i*V2V)
             ctx.lineTo(WIDTH, HEIGHT/2 - OFFSETX1 + i*V2V)
         }
         else {
-            console.log(":N",WIDTH/2 + OFFSETY1 + i*V2H)  
             ctx.moveTo(WIDTH/2 + OFFSETY1 + i*V2H, 0)
             ctx.lineTo(WIDTH/2 - OFFSETY1 + i*V2H,           HEIGHT)
         }
@@ -87,6 +82,7 @@ function transformLines() {
 }
 function axis() {
     ctx.lineWidth = 8
+    ctx.strokeStyle = AXIS_LINES
     ctx.beginPath()
     ctx.moveTo(WIDTH/2, 0)
     ctx.lineTo(WIDTH/2, HEIGHT)
@@ -100,21 +96,53 @@ function vectors() {
     ctx.strokeStyle = "#00FF00"
     ctx.beginPath()
     ctx.moveTo(WIDTH/2, HEIGHT/2)
-    ctx.lineTo(WIDTH/2 + vec1[0]*UNITX, HEIGHT/2 - vec1[2]*UNITY)
+    ctx.lineTo(WIDTH/2 + matrix[0]*UNITX, HEIGHT/2 - matrix[2]*UNITY)
     ctx.stroke()
 
     ctx.strokeStyle = "#FF0000"
     ctx.beginPath()
     ctx.moveTo(WIDTH/2, HEIGHT/2)
-    ctx.lineTo(WIDTH/2 + vec1[1]*UNITX, HEIGHT/2 - vec1[3]*UNITY)
+    ctx.lineTo(WIDTH/2 + matrix[1]*UNITX, HEIGHT/2 - matrix[3]*UNITY)
     ctx.stroke()
 }
-function matrixMultiply(vector) {
-
+function determinant() {
+    let path = new Path2D()
+    path.moveTo(WIDTH/2, HEIGHT/2)
+    path.lineTo(WIDTH/2 + matrix[0]*UNITX, HEIGHT/2 - matrix[2]*UNITY)
+    path.lineTo(WIDTH/2 + (matrix[0] + matrix[1])*UNITX , HEIGHT/2 - (matrix[2] + matrix[3])*UNITY)
+    path.lineTo(WIDTH/2 + matrix[1]*UNITX, HEIGHT/2 - matrix[3]*UNITY)
+    path.closePath()
+    ctx.fillStyle = "#ff7ae1 "
+    ctx.globalAlpha = 0.5
+    ctx.fill(path);
+    ctx.globalAlpha = 1
+    ctx.fillStyle = "#ffffff"
 }
+function refresh() {
+    matrix = [document.getElementById('0').value, document.getElementById('1').value, 
+              document.getElementById('2').value, document.getElementById('3').value]
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    backgroundLines()
+    axis()
+    transformLines()
+    vectors()
+    if (det.checked) {
+        determinant()
+    }
+}
+
+const det = document.getElementById('det')
+const enter = document.getElementById('enter')
+function newn(e) {
+    if (e.key == 'Enter') {
+        refresh()
+    }
+}
+document.getElementById('0').addEventListener('keyup',  newn)
+document.getElementById('1').addEventListener('keyup',  newn)
+document.getElementById('2').addEventListener('keyup',  newn)
+document.getElementById('3').addEventListener('keyup',  newn)
+
+det.addEventListener('change', refresh)
 ctx.lineWidth = 1
-ctx.stroke();
-backgroundLines()
-axis()
-transformLines()
-vectors()
+refresh()
