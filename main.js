3e5a5978a658db2eb214570c85b0e498b6e4a12e
joeberenshtain <107581127+joeberenshtain@users.noreply.github.com>
2023-01-01 20:25:58 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -1,66 +1,82 @@
-const WIDTH = 1000
-const HEIGHT = 1000
-const n = 15
-const f = 15
-var memMatrix, maxMatrix, ca, cb, cc, cd, running, anime
-var matrix = new Array(4);
-var count =0
+// Aesthetics:
+const BACKGROUND_COLOUR = "black"; // Background Colour
 
-const BACKGROUND = "#ffffff"
-const BACK_LINES = "#222222"
-const AXIS_LINES = "#000000"
-const TRANS_LINES = "#3f75b2"
+const AXIS_LINES_WIDTH = 2;
+const AXIS_LINES_COLOR = "#fff"; // The colour of the modified background colours. 
 
-const UNITX = WIDTH/n   
-const UNITY = HEIGHT/f
-var c = document.getElementById("canvas");
-c.width = WIDTH
-c.height = HEIGHT
-var ctx = c.getContext("2d");
+const TRANSFORMED_LINES_WIDTH = 1;
+const TRANSFORMED_LINES_COLOR = "#aaa";
 
-ctx.fillStyle = BACKGROUND;
+const BACKGROUND_LINES_WIDTH = 2;
+const BACKGROUND_LINES_COLOR = "#777"; // The Colour of the default background lines.
+
+const LINE_COUNT = 20;
+// ----------------------------------
+
+
+const WIDTH = 1000;
+const HEIGHT = 1000;
+
+const grids_x = 21;
+const grids_y = 21;
+
+var originalMatrix, newMatrix, deltaA, deltaB, deltaC, deltaD, running, animationLoop, memVector;
+var interpolatedMatrix = new Array(4);
+var interpolatedVector = new Array(2);
+var currentFrame = 0;
+
+const maxFrameCount = 50;
+
+const grid_x_width = WIDTH/grids_x; 
+const grid_y_width = HEIGHT/grids_y;
+
+const c = document.getElementById("canvas");
+const ctx = c.getContext("2d");
+c.width = WIDTH;
+c.height = HEIGHT;
+
 
 function backgroundLines() {
-    ctx.lineWidth = 1
-    ctx.strokeStyle = BACK_LINES
-    ctx.beginPath()
+    ctx.lineWidth   = BACKGROUND_LINES_WIDTH;
+    ctx.strokeStyle = BACKGROUND_LINES_COLOR;
+    ctx.beginPath();
     
-    for (let i = 0; i < n; i++) {
-        ctx.moveTo(i*UNITX+WIDTH/(2*n), 0)
-        ctx.lineTo(i*UNITX+WIDTH/(2*n), HEIGHT)
+    for (let i = 0; i < grids_x; i++) {
+        ctx.moveTo(i*grid_x_width+WIDTH/(2*grids_x), 0);
+        ctx.lineTo(i*grid_x_width+WIDTH/(2*grids_x), HEIGHT);
     } // Creates Vertical lines.
-    for (let i = 0; i < f; i++) {
-        ctx.moveTo(0,     i*UNITY+HEIGHT/(2*f))
-        ctx.lineTo(WIDTH, i*UNITY+HEIGHT/(2*f))
+    for (let i = 0; i < grids_y; i++) {
+        ctx.moveTo(0,     i*grid_y_width+HEIGHT/(2*grids_y))
+        ctx.lineTo(WIDTH, i*grid_y_width+HEIGHT/(2*grids_y))
     } // Creates horizontal lines.
    
     ctx.stroke()
 }
 function transformLines() {
-
-    const OFFSETX0 = (HEIGHT*matrix[0])/(2*matrix[2])
-    const OFFSETY0 = (WIDTH *matrix[2])/(2*matrix[0])
+    c.lineWidth = TRANSFORMED_LINES_WIDTH;
+    const OFFSETX0 = (HEIGHT*interpolatedMatrix[0])/(2*interpolatedMatrix[2]); // the gradient of the first vector making up the array.
+    const OFFSETY0 = (WIDTH *interpolatedMatrix[2])/(2*interpolatedMatrix[0]); // this is the gradient of y based on width instead so that when it is infinite (perpendicular to x axis)
     
-    const OFFSETX1 = (HEIGHT*matrix[3])/(2*matrix[1])
-    const OFFSETY1 = (WIDTH *matrix[1])/(2*matrix[3])
+    const OFFSETX1 = (HEIGHT*interpolatedMatrix[3])/(2*interpolatedMatrix[1])
+    const OFFSETY1 = (WIDTH *interpolatedMatrix[1])/(2*interpolatedMatrix[3])
 
     var V2V = 0;
     var V1V = 0;
     var V1H = 0;
-    var V2H = 0;
-    if (matrix[0]*matrix[3] - matrix[1]*matrix[2] !== 0) {
-        V2V = (matrix[2] - matrix[3]*matrix[0]/matrix[1])*UNITY
-        V1V = (matrix[3] - matrix[2]*matrix[1]/matrix[0])*UNITY
-        V1H = (matrix[1] - matrix[0]*matrix[3]/matrix[2])*UNITX
-        V2H = (matrix[0] - matrix[1]*matrix[2]/matrix[3])*UNITX
+    var V2H = 0; // what does this do? i forgot so do not touch until you figure it out
+    if (interpolatedMatrix[0]*interpolatedMatrix[3] - interpolatedMatrix[1]*interpolatedMatrix[2] !== 0) {
+        V2V = (interpolatedMatrix[2] - interpolatedMatrix[3]*interpolatedMatrix[0]/interpolatedMatrix[1])*grid_y_width
+        V1V = (interpolatedMatrix[3] - interpolatedMatrix[2]*interpolatedMatrix[1]/interpolatedMatrix[0])*grid_y_width
+        V1H = (interpolatedMatrix[1] - interpolatedMatrix[0]*interpolatedMatrix[3]/interpolatedMatrix[2])*grid_x_width
+        V2H = (interpolatedMatrix[0] - interpolatedMatrix[1]*interpolatedMatrix[2]/interpolatedMatrix[3])*grid_x_width
     }
 
-    ctx.lineWidth = 4
+    ctx.lineWidth = TRANSFORMED_LINES_WIDTH;
 
-    ctx.strokeStyle = TRANS_LINES
+    ctx.strokeStyle = TRANSFORMED_LINES_COLOR
     ctx.beginPath()
-    for (let i = -10; i < n+10; i++) {
-        if (UNITY !== Infinity && UNITY !== -Infinity && V1H !== Infinity && V1H !== -Infinity) {   
+    for (let i = -LINE_COUNT; i < grids_x+LINE_COUNT; i++) {
+        if (grid_y_width !== Infinity && grid_y_width !== -Infinity && V1H !== Infinity && V1H !== -Infinity) {   
             
             ctx.moveTo(WIDTH/2 + OFFSETX0 + i*V1H, 0)
             ctx.lineTo(WIDTH/2 - OFFSETX0 + i*V1H, HEIGHT)
@@ -70,8 +86,8 @@ function transformLines() {
             ctx.lineTo(WIDTH, HEIGHT/2 - OFFSETY0 + i*V1V)
         } // LEFT
     } // Creates Vertical lines.    
-    for (let i = -10; i < f+10; i++) {
-        if (UNITY !== Infinity && UNITY !== -Infinity && V2V !== Infinity && V2V !== -Infinity) {   
+    for (let i = -LINE_COUNT; i < grids_y+LINE_COUNT; i++) {
+        if (grid_y_width !== Infinity && grid_y_width !== -Infinity && V2V !== Infinity && V2V !== -Infinity) {   
             ctx.moveTo(0,     HEIGHT/2 + OFFSETX1 + i*V2V)
             ctx.lineTo(WIDTH, HEIGHT/2 - OFFSETX1 + i*V2V)
         }
@@ -84,8 +100,8 @@ function transformLines() {
     ctx.stroke()
 }
 function axis() {
-    ctx.lineWidth = 8
-    ctx.strokeStyle = AXIS_LINES
+    ctx.lineWidth   = AXIS_LINES_WIDTH;
+    ctx.strokeStyle = AXIS_LINES_COLOR;
     ctx.beginPath()
     ctx.moveTo(WIDTH/2, 0)
     ctx.lineTo(WIDTH/2, HEIGHT)
@@ -94,75 +110,109 @@ function axis() {
     ctx.stroke()
 }
 function vectors() {
-    ctx.lineWidth = 4
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = "#00FF00";
 
-    ctx.strokeStyle = "#00FF00"
     ctx.beginPath()
     ctx.moveTo(WIDTH/2, HEIGHT/2)
-    ctx.lineTo(WIDTH/2 + matrix[0]*UNITX, HEIGHT/2 - matrix[2]*UNITY)
+    ctx.lineTo(WIDTH/2 + interpolatedMatrix[0]*grid_x_width, HEIGHT/2 - interpolatedMatrix[2]*grid_y_width)
     ctx.stroke()
 
     ctx.strokeStyle = "#FF0000"
     ctx.beginPath()
     ctx.moveTo(WIDTH/2, HEIGHT/2)
-    ctx.lineTo(WIDTH/2 + matrix[1]*UNITX, HEIGHT/2 - matrix[3]*UNITY)
+    ctx.lineTo(WIDTH/2 + interpolatedMatrix[1]*grid_x_width, HEIGHT/2 - interpolatedMatrix[3]*grid_y_width)
     ctx.stroke()
 }
 function determinant() {
     let path = new Path2D()
     path.moveTo(WIDTH/2, HEIGHT/2)
-    path.lineTo(WIDTH/2 + matrix[0]*UNITX, HEIGHT/2 - matrix[2]*UNITY)
-    path.lineTo(WIDTH/2 + (matrix[0] + matrix[1])*UNITX , HEIGHT/2 - (matrix[2] + matrix[3])*UNITY)
-    path.lineTo(WIDTH/2 + matrix[1]*UNITX, HEIGHT/2 - matrix[3]*UNITY)
+    path.lineTo(WIDTH/2 + interpolatedMatrix[0]*grid_x_width, HEIGHT/2 - interpolatedMatrix[2]*grid_y_width)
+    path.lineTo(WIDTH/2 + (interpolatedMatrix[0] + interpolatedMatrix[1])*grid_x_width , HEIGHT/2 - (interpolatedMatrix[2] + interpolatedMatrix[3])*grid_y_width)
+    path.lineTo(WIDTH/2 + interpolatedMatrix[1]*grid_x_width, HEIGHT/2 - interpolatedMatrix[3]*grid_y_width)
     path.closePath()
     ctx.fillStyle = "#ff7ae1 "
-    ctx.globalAlpha = 0.5
+    ctx.globalAlpha = 0.5;
     ctx.fill(path);
-    ctx.globalAlpha = 1
+    ctx.globalAlpha = 1;
     ctx.fillStyle = "#ffffff"
 }
+function vector() {
+    ctx.lineWidth = 4;  
+    ctx.strokeStyle = "#002277";
 
-function update() {
-    count++
-    if (count >=20) {
-        clearInterval(anime);
-        count=20
-        running = false
-        
-    }
+    ctx.beginPath();
+    ctx.moveTo(WIDTH/2 + memVector[0]*grid_x_width - 10, HEIGHT/2 - memVector[1]*grid_y_width- 10);
+    ctx.lineTo(WIDTH/2 + memVector[0]*grid_x_width + 10, HEIGHT/2 - memVector[1]*grid_y_width + 10);
+    
+    ctx.moveTo(WIDTH/2 + memVector[0]*grid_x_width + 10, HEIGHT/2 - memVector[1]*grid_y_width- 10);
+    ctx.lineTo(WIDTH/2 + memVector[0]*grid_x_width - 10, HEIGHT/2 - memVector[1]*grid_y_width + 10);
+    ctx.stroke();
+    ctx.strokeStyle = "#0f0";
+
+    ctx.beginPath();
+
+
+    ctx.moveTo(WIDTH/2 + interpolatedVector[0]*grid_x_width - 10, HEIGHT/2 - interpolatedVector[1]*grid_y_width- 10);
+    ctx.lineTo(WIDTH/2 + interpolatedVector[0]*grid_x_width + 10, HEIGHT/2 - interpolatedVector[1]*grid_y_width + 10);
+    
+    ctx.moveTo(WIDTH/2 + interpolatedVector[0]*grid_x_width + 10, HEIGHT/2 - interpolatedVector[1]*grid_y_width- 10);
+    ctx.lineTo(WIDTH/2 + interpolatedVector[0]*grid_x_width - 10, HEIGHT/2 - interpolatedVector[1]*grid_y_width + 10);
+    ctx.stroke();
+}
+function update(frame) {
+    if (frame >= maxFrameCount) {
+        clearInterval(animationLoop);
+        frame = maxFrameCount;
+        running = false;
+    }   
   
     
-    matrix[0] = memMatrix[0]+ca*count;
-    matrix[1] = memMatrix[1]+cb*count;
-    matrix[2] = memMatrix[2]+cc*count;
-    matrix[3] = memMatrix[3]+cd*count;
+    interpolatedMatrix[0] = originalMatrix[0]+deltaA*frame;
+    interpolatedMatrix[1] = originalMatrix[1]+deltaB*frame;
+    interpolatedMatrix[2] = originalMatrix[2]+deltaC*frame;
+    interpolatedMatrix[3] = originalMatrix[3]+deltaD*frame;
 
+    interpolatedVector[0] = memVector[0]*interpolatedMatrix[0] + memVector[1]*interpolatedMatrix[1];
+    interpolatedVector[1] = memVector[0]*interpolatedMatrix[2] + memVector[1]*interpolatedMatrix[3];
+
+    ctx.fillStyle = BACKGROUND_COLOUR;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    backgroundLines()
-    axis()
-    transformLines()
-    vectors()
+    backgroundLines();
+    axis();
+    transformLines();
+    vectors();
+    vector();
+    
     if (det.checked) {
         determinant()
     }
     
 }
 
-
 function animate() {    
     if (running) return;
-    count = 0
-    memMatrix = maxMatrix || [1, 0, 0, 1]
+    running = true;
 
-    maxMatrix = [parseInt(document.getElementById('0').value), parseInt(document.getElementById('1').value), 
-                 parseInt(document.getElementById('2').value), parseInt(document.getElementById('3').value)]
-     ca = (maxMatrix[0]-memMatrix[0])/20
-     cb = (maxMatrix[1]-memMatrix[1])/20
-     cc = (maxMatrix[2]-memMatrix[2])/20
-     cd = (maxMatrix[3]-memMatrix[3])/20
-    running = true
-    anime = setInterval(update, 50)
+    currentFrame = 0;
+    originalMatrix = newMatrix || [1, 0, 0, 1];
+
+    newMatrix = [parseFloat(document.getElementById('0').value), parseFloat(document.getElementById('1').value), 
+                 parseFloat(document.getElementById('2').value), parseFloat(document.getElementById('3').value)];
+    // ^ The final matrix before the transformation.
+    // 0 1
+    // 2 3
+
+    memVector = [parseFloat(document.getElementById('4').value), parseFloat(document.getElementById('5').value)];   
+    maxVector = [memVector[0]*newMatrix[0] + memVector[1]*newMatrix[1], memVector[0]*newMatrix[2] + memVector[1]*newMatrix[3]]
+
+    deltaA = (newMatrix[0]-originalMatrix[0])/maxFrameCount; // _______
+    deltaB = (newMatrix[1]-originalMatrix[1])/maxFrameCount; // | A B |
+    deltaC = (newMatrix[2]-originalMatrix[2])/maxFrameCount; // | C D |
+    deltaD = (newMatrix[3]-originalMatrix[3])/maxFrameCount; // ͞ ͞ ͞ ͞ ͞ ͞ ͞ 
+
+    animationLoop = setInterval(() => {update(currentFrame); currentFrame++;}, 1000/maxFrameCount);
 }
-document.getElementById('up').addEventListener('click', animate)
-document.getElementById('det').addEventListener('change', update)
-animate()
+document.getElementById ('up').addEventListener('click', animate);
+document.getElementById('det').addEventListener('change', ()=>{});
+animate();
